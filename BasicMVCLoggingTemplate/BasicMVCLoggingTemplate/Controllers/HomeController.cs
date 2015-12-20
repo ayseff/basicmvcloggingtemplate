70aeda872eea34b99a82fa1b9ff9c2c0a77441dd
Nuwan Wijayaweera <nuwan.w@outlook.com.au>
2015-12-20 13:43:11 +1100

    * SQLite Integration for Elmah * Added 2 Menu links (Elmah logs page and Configure Glimpse)

--- BasicMVCLoggingTemplate/BasicMVCLoggingTemplate/Controllers/HomeController.cs
+++ BasicMVCLoggingTemplate/BasicMVCLoggingTemplate/Controllers/HomeController.cs
@@ -9,11 +9,14 @@ namespace BasicMVCLoggingTemplate.Controllers
 {
     public class HomeController : Controller
     {
+        // Instance of the logger ( Add this to each and every class you write )
         private static Logger logger = LogManager.GetCurrentClassLogger();
 
         // GET: Home
+        // Try refreshing the home page and see how elmah (~/elmah) repeatedly adds all the logs
         public ActionResult Index()
         {
+            // Example Logs
             logger.Trace("Sample trace message");
             logger.Debug("Sample debug message");
             logger.Info("Sample informational message");
@@ -21,7 +24,7 @@ namespace BasicMVCLoggingTemplate.Controllers
             logger.Error("Sample error message");
             logger.Fatal("Sample fatal error message");
 
-            // alternatively you can call the Log() method 
+            // Alternatively you can call the Log() method 
             // and pass log level as the parameter.
             logger.Log(LogLevel.Info, "Sample informational message");
 
