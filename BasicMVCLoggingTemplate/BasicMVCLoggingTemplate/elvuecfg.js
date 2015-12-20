0d1bf260ce2dccfaa0c4efc0588f88d1b04a6df2
Nuwan Wijayaweera <nuwan.w@outlook.com.au>
2015-12-20 14:45:26 +1100

    Elvue integration for Elmah

---
+++ BasicMVCLoggingTemplate/BasicMVCLoggingTemplate/elvuecfg.js
@@ -0,0 +1,5 @@
+config = {    
+    title: 'Elmah Logs',        // report title
+    /*limit: 250,               // limit report to these most recent errors */
+    src: '/Elmah/download'   // location of log download URL
+};
